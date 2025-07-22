7eebad9072745536c62f2ef07b1362c00eb9993a
eletrixosalfa <eletrixosalfa@gmail.com>
2025-07-22 10:31:28 +0100

    frontend atualizado 10:30 22/07

---
+++ frontend/ulumin-app/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import Header from './components/Header';
+
+// Dentro do componente principal:
+<Header />
+
+
+export default function App() {
+  return (
+    <Header />
+  );
+}

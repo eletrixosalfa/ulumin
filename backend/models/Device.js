49fc16df9710d989a276a87a8f13ef37b9f87fbc
eletrixosalfa <eletrixosalfa@gmail.com>
2025-08-14 12:57:58 +0100

    tudo atualizado 14/08

--- backend/models/Device.js
+++ backend/models/Device.js
@@ -1,20 +1,23 @@
 const mongoose = require('mongoose');
 
-const deviceSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  icon: { type: String, default: 'devices' },
-  status: { type: String, enum: ['on', 'off'], default: 'off' },
-  ipAddress: { type: String },
-  category: { type: mongoose.Schema.Types.ObjectId, ref: 'Category', required: true },
-  room: { type: mongoose.Schema.Types.ObjectId, ref: 'Room', required: true },
-  owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-
-  actions: [
-    {
-      name: { type: String, required: true },
-      command: { type: String }, 
-    },
-  ],
-}, { timestamps: true });
+const deviceSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    icon: { type: String, default: 'devices' },
+    status: { type: String, enum: ['on', 'off'], default: 'off' },
+    ipAddress: { type: String, default: '' },
+    mqttId: { type: String, default: '' },
+    model: { type: String, default: '', required: true}, 
+    room: { type: mongoose.Schema.Types.ObjectId, ref: 'Room', required: true },
+    owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    actions: [
+      {
+        name: { type: String, required: true },
+        command: { type: String, default: '' },
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model('Device', deviceSchema);

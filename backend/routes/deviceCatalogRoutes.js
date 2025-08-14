078c36d93978381fc05f07f49ccf363f626e1546
eletrixosalfa <eletrixosalfa@gmail.com>
2025-08-14 11:45:58 +0100

    tudo atualizado 14/08

--- backend/routes/deviceCatalogRoutes.js
+++ backend/routes/deviceCatalogRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const deviceCatalogController = require('../controllers/deviceCatalogController');
+const deviceCatalogController = require('../controllers/devicecatalogController');
 
 router.get('/model/:model', deviceCatalogController.getActionsByModel);
 router.get('/', deviceCatalogController.getAllCatalogDevices);

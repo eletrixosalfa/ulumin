668b26576b3818cb3b6558965782d8aef530c8e3
eletrixosalfa <eletrixosalfa@gmail.com>
2025-08-14 14:15:16 +0100

    tudo atualizado 14/08

--- backend/models/Schedule.js
+++ backend/models/Schedule.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose');
+const deviceCatalog = require('./DeviceCatalog');
+
+// Junta todas as ações de todos os modelos em um único array
+const allActions = deviceCatalog.flatMap(device => device.actions);
 
 const scheduleSchema = new mongoose.Schema({
   device: { type: mongoose.Schema.Types.ObjectId, ref: 'Device', required: true },
-  action: { type: String, enum: ['on', 'off'], required: true },
-  time: { type: String, required: true }, // HH:mm formato
+  action: { type: String, enum: allActions, required: true },
+  time: { type: String, required: true }, // HH:mm
   repeat: { type: [String], enum: ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'] },
   owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
 }, { timestamps: true });
